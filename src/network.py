fef5f40b427bdca6d1a1512b4b4db9bdfaa455f1
Mohseen Mukaddam <mohseenmukaddam6@gmail.com>
2017-11-20 14:57:13 -0700

    Normalize

--- src/network.py
+++ src/network.py
@@ -1,5 +1,3 @@
-import cPickle as pickle
-import gzip
 import os
 import random
 import pdb
@@ -7,7 +5,7 @@ import pdb
 import numpy as np
 from mnist import MNIST
 
-class Network(object):
+class Neural_Network(object):
     def __init__(self):
         np.random.seed(0)
         self.biases = []
@@ -19,8 +17,8 @@ class Network(object):
         self.weights.append(np.random.randn(256, 10))
         self.biases.append(np.random.randn(1, 10))
 
-    def feedforward(self, a):
-        scaled_a = np.true_divide(a,1)
+    def feed_Network(self, a):
+        scaled_a = np.true_divide(a, 255)
         z1 = (scaled_a.transpose()).dot(self.weights[0]) + self.biases[0]
         m2 = np.random.binomial(1, 1, size=z1.shape)
         a1 = sigmoid(z1)*m2
@@ -32,49 +30,54 @@ class Network(object):
         probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
         return (probs == probs.max(axis=1, keepdims=1)).astype(int)
 
-    def SGD(self, training_data, epochs, mini_batch_size, eta):
+    def test_Network(self, testing_data):
+        test_results = [(np.argmax(self.feed_Network(x)), y) for (x, y) in testing_data]
+        return sum(int(x == y) for (x, y) in test_results)
 
-        if test_data: n_test = len(test_data)
+    def Stochastic_Gradient_Descent(self, training_data, learning_rate):
+
+        iterations = 3
+        SGD_Size = 10
+        if testing_data: n_test = len(testing_data)
         n = len(training_data)
-        for j in xrange(epochs):
+        for j in xrange(iterations):
             random.shuffle(training_data)
-            mini_batches = [
-                training_data[k:k + mini_batch_size]
-                for k in xrange(0, n, mini_batch_size)
+            SGD_Batches = [
+                training_data[k:k + SGD_Size]
+                for k in xrange(0, n, SGD_Size)
             ]
-            for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
-            if test_data:
-                print "Epoch {0}: {1} / {2}".format(
-                    j, self.evaluate(test_data), n_test)
+            for SGD_Batch in SGD_Batches:
+                self.update_SGD_Batch(SGD_Batch, learning_rate)
+            if testing_data:
+                print "iteration {0}:  accuracy={1}%".format(j, np.true_divide(self.test_Network(testing_data),n_test)*100)
             else:
-                print "Epoch {0} complete".format(j)
+                print "iteration {0} complete".format(j)
 
-    def update_weights(self, weight, eta, length, nabla):
-        return weight - ((eta / length) * nabla)
+    def update_weights(self, weight, learning_rate, length, nabla):
+        return weight - ((learning_rate / length) * nabla)
 
-    def update_mini_batch(self, mini_batch, eta):
+    def update_SGD_Batch(self, SGD_Batch, learning_rate):
 
-        nabla_b = [np.zeros(b.shape) for b in self.biases]
-        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        batch_bias = [np.zeros(b.shape) for b in self.biases]
+        batch_weight = [np.zeros(w.shape) for w in self.weights]
         
-        for x, y in mini_batch:
-            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-            nabla_w[0] += delta_nabla_w[0].transpose()
-            nabla_w[1] += delta_nabla_w[1].transpose()
-            nabla_w[2] += delta_nabla_w[2]
-            nabla_b[0] += delta_nabla_b[0]
-            nabla_b[1] += delta_nabla_b[1]
-            nabla_b[2] += delta_nabla_b[2]
+        for dig, op in SGD_Batch:
+            delta_bias, delta_weight = self.back_Propogation(dig, op)
+            batch_weight[0] += delta_weight[0].transpose()
+            batch_weight[1] += delta_weight[1].transpose()
+            batch_weight[2] += delta_weight[2]
+            batch_bias[0] += delta_bias[0]
+            batch_bias[1] += delta_bias[1]
+            batch_bias[2] += delta_bias[2]
 
         # Functional approach
         self.weights = list(
             map(
                 lambda (i, x): self.update_weights(
                     x,
-                    eta,
-                    len(mini_batch),
-                    nabla_w[i]
+                    learning_rate,
+                    len(SGD_Batch),
+                    batch_weight[i]
                 ),
                 enumerate(self.weights)
             )
@@ -83,64 +86,57 @@ class Network(object):
             map(
                 lambda (i, x): self.update_weights(
                     x,
-                    eta,
-                    len(mini_batch),
-                    nabla_b[i]
+                    learning_rate,
+                    len(SGD_Batch),
+                    batch_bias[i]
                 ),
                 enumerate(self.biases)
             )
         )
 
-    def backprop(self, x, y):
+    def back_Propogation(self, dig, op):
 
-        nabla_b = [np.zeros(b.shape) for b in self.biases]
-        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        batch_bias = [np.zeros(b.shape) for b in self.biases]
+        batch_weight = [np.zeros(w.shape) for w in self.weights]
 
-        # feedforward
-        scaled_x = np.true_divide(x,255)
-        #pdb.set_trace()
-        activation = scaled_x
+        scaled_x = np.true_divide(dig,255)
+        activation_fn = scaled_x
         activations = [scaled_x]
-        zs = []
+        activation_layer = []
 
-        z = np.dot(activation.transpose(),self.weights[0]) + self.biases[0]
-        zs.append(z)
+        activation= np.dot(activation_fn.transpose(),self.weights[0]) + self.biases[0]
+        activation_layer.append(activation)
 
-        activation = sigmoid(z)
-        activations.append(activation)
+        activation_fn = sigmoid(activation)
+        activations.append(activation_fn)
 
-        z = np.dot(activation,self.weights[1]) + self.biases[1]
-        zs.append(z)
-        activation = sigmoid(z)
-        activations.append(activation)
+        activation = np.dot(activation_fn,self.weights[1]) + self.biases[1]
+        activation_layer.append(activation)
+        activation_fn = sigmoid(activation)
+        activations.append(activation_fn)
 
-        z = np.dot(activation,self.weights[2])+self.biases[2]
-        zs.append(z)
-        exp_scores = np.exp(z)
+        activation = np.dot(activation_fn,self.weights[2])+self.biases[2]
+        activation_layer.append(activation)
+        exp_scores = np.exp(activation)
         probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
         activations.append(probs)
 
         # backward pass
 
-        delta = activations[3] - y.transpose()
-        nabla_w[2] = (activations[2].transpose()).dot(delta)
-        nabla_b[2] = np.sum(delta, axis=0, keepdims=True)
+        gradient = activations[3] - op.transpose()
+        batch_weight[2] = (activations[2].transpose()).dot(gradient)
+        batch_bias[2] = np.sum(gradient, axis=0, keepdims=True)
 
-        sp = sigmoid(zs[-2],True)
-        delta = np.dot(self.weights[2], delta.transpose()).transpose() * sp
-        nabla_b[1] = delta
-        nabla_w[1] = np.dot(delta, activations[1].transpose())
+        sigmoid_prime = sigmoid(activation_layer[-2],True)
+        gradient = np.dot(self.weights[2], gradient.transpose()).transpose() * sigmoid_prime
+        batch_bias[1] = gradient
+        batch_weight[1] = np.dot(gradient, activations[1].transpose())
 
-        sp = sigmoid(zs[-3],True)
-        delta = np.dot(self.weights[1], delta.transpose()).transpose() * sp
-        nabla_b[0] = delta
-        nabla_w[0] = np.dot(delta.transpose(), activations[0].transpose())
-        #pdb.set_trace()
-        return (nabla_b, nabla_w)
-
-    def evaluate(self, test_data):
-        test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
-        return sum(int(x == y) for (x, y) in test_results)
+        sigmoid_prime = sigmoid(activation_layer[-3],True)
+        gradient = np.dot(self.weights[1], gradient.transpose()).transpose() * sigmoid_prime
+        batch_bias[0] = gradient
+        batch_weight[0] = np.dot(gradient.transpose(), activations[0].transpose())
+        return (batch_bias, batch_weight)
 
 def sigmoid(z,derivative = False):
     if derivative:
@@ -148,13 +144,12 @@ def sigmoid(z,derivative = False):
     else:
         return 1.0/(1.0+np.exp(-z))
 
-class MNIST1(object):
+class MNIST_load_data(object):
     def __init__(self):
         pass
 
     def load_data_wrapper(self):
 
-        #change path
         data_path = os.path.abspath(
             os.path.join(
                 os.path.dirname(__file__),
@@ -165,45 +160,27 @@ class MNIST1(object):
         mndata = MNIST(data_path)
         train = mndata.load_training()
         test = mndata.load_testing()
-        """" pdb.set_trace()
-
-               file_path = os.path.abspath(
-                   os.path.join(
-                       os.path.dirname(__file__),
-                       '..',
-                       'data',
-                       'mnist.pkl.gz'
-                   )
-               )
-
-               f = gzip.open(file_path, 'rb')
-               tr_d, va_d, te_d = pickle.load(f)
-               f.close()"""
 
         training_inputs = [np.reshape(x, (784, 1)) for x in train[0]]
-        training_results = [self.vectorized_result(y) for y in train[1]]
+        training_results = [self.one_hot_vectors(y) for y in train[1]]
         training_data = zip(training_inputs, training_results)
-        #validation_inputs = [np.reshape(x, (784, 1)) for x in test[0]]
-        #validation_data = zip(validation_inputs, test[1])
         test_inputs = [np.reshape(x, (784, 1)) for x in test[0]]
         inputs = [test_inputs[i:i + 5000] for i in xrange(0, len(test_inputs),5000)]
         labels = [test[1][i:i + 5000] for i in xrange(0, len(test[1]), 5000)]
-        test_data = zip(inputs[0], labels[0])
+        testing_data = zip(inputs[0], labels[0])
         validation_data = zip(inputs[1], labels[1])
 
-        return (training_data, validation_data, test_data)
+        return (training_data, validation_data, testing_data)
 
 
-
-
-    def vectorized_result(self, j):
+    def one_hot_vectors(self, j):
         e = np.zeros((10, 1))
         e[j] = 1.0
         return e
 
 if __name__ == "__main__":
-    mnist = MNIST1()
-    training_data, validation_data, test_data = mnist.load_data_wrapper()
-    net = Network()
-    net.SGD(training_data, 3, 10, 0.01)
+    mnist = MNIST_load_data()
+    training_data, validation_data, testing_data = mnist.load_data_wrapper()
+    network = Neural_Network()
+    network.Stochastic_Gradient_Descent(training_data, 0.01)
     
