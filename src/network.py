a05245daaf29687bee2e03a2ddb216cf60b15fa3
Nagarchith <nagarchith@gmail.com>
2017-11-21 19:18:48 -0700

    validation,accuracy and error

--- src/network.py
+++ src/network.py
@@ -6,7 +6,9 @@ import numpy as np
 from mnist import MNIST
 
 class Neural_Network(object):
+
     def __init__(self):
+
         np.random.seed(0)
         self.biases = []
         self.weights = []
@@ -18,6 +20,7 @@ class Neural_Network(object):
         self.biases.append(np.random.randn(1, 10))
 
     def feed_Network(self, a):
+
         scaled_a = np.true_divide(a, 255)
         z1 = (scaled_a.transpose()).dot(self.weights[0]) + self.biases[0]
         m2 = np.random.binomial(1, 1, size=z1.shape)
@@ -28,18 +31,31 @@ class Neural_Network(object):
         z3 = a2.dot(self.weights[2]) + self.biases[2]
         exp_scores = np.exp(z3)
         probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-        return (probs == probs.max(axis=1, keepdims=1)).astype(int)
+        #return (probs == probs.max(axis=1, keepdims=1)).astype(int)
+        return probs
 
     def test_Network(self, testing_data):
-        test_results = [(np.argmax(self.feed_Network(x)), y) for (x, y) in testing_data]
-        return sum(int(x == y) for (x, y) in test_results)
+        total_error = 0
+        test_results = 0
+        mnist = MNIST_load_data()
+        for (x,y) in testing_data:
+            probs = self.feed_Network(x)
+            logprob = -np.log(probs)
+            total_error += np.dot(logprob,mnist.one_hot_vectors(y))
+            if(np.argmax(probs) == y):
+                test_results += 1
+        #test_results = [(np.argmax(self.feed_Network(x)), y) for (x, y) in testing_data]
+        #pdb.set_trace()
+        return test_results,total_error
 
-    def Stochastic_Gradient_Descent(self, training_data, learning_rate):
+    def Stochastic_Gradient_Descent(self, validation_data, training_data, learning_rate):
 
         iterations = 3
         SGD_Size = 10
-        if testing_data: n_test = len(testing_data)
+        if testing_data: n_test = len(validation_data)
         n = len(training_data)
+        error = 999
+        i=0
         for j in xrange(iterations):
             random.shuffle(training_data)
             SGD_Batches = [
@@ -48,12 +64,20 @@ class Neural_Network(object):
             ]
             for SGD_Batch in SGD_Batches:
                 self.update_SGD_Batch(SGD_Batch, learning_rate)
-            if testing_data:
-                print "iteration {0}:  accuracy={1}%".format(j, np.true_divide(self.test_Network(testing_data),n_test)*100)
-            else:
-                print "iteration {0} complete".format(j)
+            if validation_data:
+                previous_error = error
+                accuracy,error = self.test_Network(validation_data)
+                error = np.true_divide(error,n_test)
+                pdb.set_trace()
+                print "Validation: iteration {0}:  accuracy={1}% with error {2}".format(j, np.true_divide(accuracy,n_test)*100, error)
+                if(previous_error < error):
+                    break
+
+        accuracy, error = self.test_Network(testing_data)
+        print "Testing:  accuracy={1}% with error {2}".format(j, np.true_divide(accuracy, n_test) * 100,np.true_divide(error, n_test))
 
     def update_weights(self, weight, learning_rate, length, nabla):
+
         return weight - ((learning_rate / length) * nabla)
 
     def update_SGD_Batch(self, SGD_Batch, learning_rate):
@@ -163,7 +187,11 @@ class MNIST_load_data(object):
 
         training_inputs = [np.reshape(x, (784, 1)) for x in train[0]]
         training_results = [self.one_hot_vectors(y) for y in train[1]]
-        training_data = zip(training_inputs, training_results)
+        # training for only 10000 data set
+        training_inputs = [training_inputs[i:i + 10000] for i in xrange(0, len(training_inputs), 10000)]
+        training_results = [training_results[i:i + 10000] for i in xrange(0, len(training_results), 10000)]
+        training_data = zip(training_inputs[0], training_results[0])
+        #pdb.set_trace()
         test_inputs = [np.reshape(x, (784, 1)) for x in test[0]]
         inputs = [test_inputs[i:i + 5000] for i in xrange(0, len(test_inputs),5000)]
         labels = [test[1][i:i + 5000] for i in xrange(0, len(test[1]), 5000)]
@@ -179,8 +207,9 @@ class MNIST_load_data(object):
         return e
 
 if __name__ == "__main__":
+
     mnist = MNIST_load_data()
     training_data, validation_data, testing_data = mnist.load_data_wrapper()
     network = Neural_Network()
-    network.Stochastic_Gradient_Descent(training_data, 0.01)
+    network.Stochastic_Gradient_Descent(validation_data, training_data, 0.01)
     
