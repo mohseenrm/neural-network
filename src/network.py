b6b845b6335d61819cc623344bb4d9859bbf63b4
Nagarchith <nagarchith@gmail.com>
2017-11-24 13:07:23 -0700

    added training Error

--- src/network.py
+++ src/network.py
@@ -55,26 +55,30 @@ class Neural_Network(object):
         if testing_data: n_test = len(validation_data)
         n = len(training_data)
         error = 999
-        i=0
+
         for j in xrange(iterations):
             random.shuffle(training_data)
             SGD_Batches = [
                 training_data[k:k + SGD_Size]
                 for k in xrange(0, n, SGD_Size)
             ]
+            total_error = 0
             for SGD_Batch in SGD_Batches:
-                self.update_SGD_Batch(SGD_Batch, learning_rate)
+                total_error += self.update_SGD_Batch(SGD_Batch, learning_rate)
+
+            print 'Training Error:' , total_error[0][0]/len(training_data)
+
             if validation_data:
                 previous_error = error
                 accuracy,error = self.test_Network(validation_data)
                 error = np.true_divide(error,n_test)
-                pdb.set_trace()
-                print "Validation: iteration {0}:  accuracy={1}% with error {2}".format(j, np.true_divide(accuracy,n_test)*100, error)
+                #pdb.set_trace()
+                print "Validation: iteration {0}:  accuracy={1}% with error {2}".format(j, np.true_divide(accuracy,n_test)*100, error[0][0])
                 if(previous_error < error):
                     break
 
         accuracy, error = self.test_Network(testing_data)
-        print "Testing:  accuracy={1}% with error {2}".format(j, np.true_divide(accuracy, n_test) * 100,np.true_divide(error, n_test))
+        print "Testing:  accuracy={1}% with error {2}".format(j, np.true_divide(accuracy, n_test) * 100,np.true_divide(error[0][0], n_test))
 
     def update_weights(self, weight, learning_rate, length, nabla):
 
@@ -84,9 +88,10 @@ class Neural_Network(object):
 
         batch_bias = [np.zeros(b.shape) for b in self.biases]
         batch_weight = [np.zeros(w.shape) for w in self.weights]
-        
+        total_error = 0
         for dig, op in SGD_Batch:
-            delta_bias, delta_weight = self.back_Propogation(dig, op)
+            delta_bias, delta_weight, error = self.back_Propogation(dig, op)
+            total_error += error
             batch_weight[0] += delta_weight[0].transpose()
             batch_weight[1] += delta_weight[1].transpose()
             batch_weight[2] += delta_weight[2]
@@ -118,6 +123,8 @@ class Neural_Network(object):
             )
         )
 
+        return total_error
+
     def back_Propogation(self, dig, op):
 
         batch_bias = [np.zeros(b.shape) for b in self.biases]
@@ -143,6 +150,8 @@ class Neural_Network(object):
         activation_layer.append(activation)
         exp_scores = np.exp(activation)
         probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+        logprob = -np.log(probs)
+        error = np.dot(logprob, op)
         activations.append(probs)
 
         # backward pass
@@ -160,7 +169,7 @@ class Neural_Network(object):
         gradient = np.dot(self.weights[1], gradient.transpose()).transpose() * sigmoid_prime
         batch_bias[0] = gradient
         batch_weight[0] = np.dot(gradient.transpose(), activations[0].transpose())
-        return (batch_bias, batch_weight)
+        return (batch_bias, batch_weight,error)
 
 def sigmoid(z,derivative = False):
     if derivative:
@@ -188,8 +197,8 @@ class MNIST_load_data(object):
         training_inputs = [np.reshape(x, (784, 1)) for x in train[0]]
         training_results = [self.one_hot_vectors(y) for y in train[1]]
         # training for only 10000 data set
-        training_inputs = [training_inputs[i:i + 10000] for i in xrange(0, len(training_inputs), 10000)]
-        training_results = [training_results[i:i + 10000] for i in xrange(0, len(training_results), 10000)]
+        training_inputs = [training_inputs[i:i + 40000] for i in xrange(0, len(training_inputs), 40000)]
+        training_results = [training_results[i:i + 40000] for i in xrange(0, len(training_results), 40000)]
         training_data = zip(training_inputs[0], training_results[0])
         #pdb.set_trace()
         test_inputs = [np.reshape(x, (784, 1)) for x in test[0]]
