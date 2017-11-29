8c486f36a1388f7052ada0439dbb4694b6293d7d
Nagarchith <nagarchith@gmail.com>
2017-11-28 22:48:23 -0700

    gradient checker

--- src/Gradient_Check.py
+++ src/Gradient_Check.py
@@ -1,5 +1,5 @@
-""" This program slightly  incorporates ideas from tutorial blog and implementation of neural
-    network by Michael Nielsen(http://neuralnetworksanddeeplearning.com/chap1.html)"""
+""" This program slightly incorporates ideas from tutorial blog and implementation of neural
+    network by Ottokar Tilk(https://gist.github.com/ottokart/ebd3d32438c13a62ea3c)"""
 
 import os
 import random
@@ -33,103 +33,6 @@ class Neural_Network(object):
         probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
         return probs
 
-    def test_Network(self, testing_data,dropout=1):
-        total_error = 0
-        test_results = 0
-        mnist = MNIST_load_data()
-        for (x,y) in testing_data:
-            probs = self.feed_Network(x,dropout)
-            logprob = -np.log(probs)
-            op = mnist.one_hot_vectors(y)
-            total_error += np.dot(logprob, op)
-            if(np.argmax(probs) == y):
-                test_results += 1
-        return test_results,total_error
-
-    def Stochastic_Gradient_Descent(self, validation_data, training_data, testing_data, learning_rate, dropout):
-
-        if dropout == 1:
-            iterations = 30
-        else:
-            iterations = 100
-        SGD_Size = 10
-        n_val = len(validation_data)
-        n_test = len(testing_data)
-        n = len(training_data)
-        error = 999
-
-        for j in xrange(iterations):
-            m2 = np.random.binomial(1, dropout, size=(1, 256))
-            m3 = np.random.binomial(1, dropout, size=(1, 256))
-            random.shuffle(training_data)
-            SGD_Batches = [
-                training_data[k:k + SGD_Size]
-                for k in xrange(0, n, SGD_Size)
-            ]
-            total_error = 0
-            for SGD_Batch in SGD_Batches:
-                total_error += self.update_SGD_Batch(SGD_Batch, learning_rate, m2,m3)
-
-            #print 'Training Error:' , total_error[0][0]/len(training_data)
-
-            if validation_data:
-                previous_error = error
-                accuracy,error = self.test_Network(validation_data,dropout)
-                error = np.true_divide(error,n_val)
-                print "Validation: iteration {0}:  accuracy={1}% with Cost {2}".format(j, np.true_divide(accuracy,n_val)*100, error[0][0])
-                #accuracy1, error1 = self.test_Network(testing_data,dropout)
-                #print "Testing:  accuracy={0}% with cost {1}".format(np.true_divide(accuracy1, n_test) * 100, np.true_divide(error1[0][0], n_test))
-                if dropout == 1:
-                    if(previous_error < error):
-                        break
-
-        accuracy, error = self.test_Network(testing_data,dropout)
-        print "Testing:  accuracy={1}% with cost {2}".format(j, np.true_divide(accuracy, n_test) * 100,np.true_divide(error[0][0], n_test))
-
-    def update_values(self, old_values, learning_rate, length, new_values):
-
-        return old_values - ((learning_rate / length) * new_values)
-
-    def update_SGD_Batch(self, SGD_Batch, learning_rate, m2,m3):
-
-        batch_bias = [np.zeros(b.shape) for b in self.biases]
-        batch_weight = [np.zeros(w.shape) for w in self.weights]
-        total_error = 0
-        for dig, op in SGD_Batch:
-            delta_bias, delta_weight, error = self.back_Propogation(dig, op, m2, m3)
-            total_error += error
-            batch_weight[0] += delta_weight[0].transpose()
-            batch_weight[1] += delta_weight[1].transpose()
-            batch_weight[2] += delta_weight[2]
-            batch_bias[0] += delta_bias[0]
-            batch_bias[1] += delta_bias[1]
-            batch_bias[2] += delta_bias[2]
-
-        # Functional approach
-        self.weights = list(
-            map(
-                lambda (i, x): self.update_values(
-                    x,
-                    learning_rate,
-                    len(SGD_Batch),
-                    batch_weight[i]
-                ),
-                enumerate(self.weights)
-            )
-        )
-        self.biases = list(
-            map(
-                lambda (i, x): self.update_values(
-                    x,
-                    learning_rate,
-                    len(SGD_Batch),
-                    batch_bias[i]
-                ),
-                enumerate(self.biases)
-            )
-        )
-
-        return total_error
 
     def back_Propogation(self, dig, op, m2, m3):
 
@@ -157,8 +60,6 @@ class Neural_Network(object):
         activation_layer.append(activation)
         exp_scores = np.exp(activation)
         probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-        logprob = -np.log(probs)
-        error = np.dot(logprob, op)
         activations.append(probs)
 
         # backward pass
@@ -177,7 +78,49 @@ class Neural_Network(object):
         batch_bias[0] = gradient
         batch_weight[0] = np.dot(gradient.transpose(), activations[0].transpose())
 
-        return (batch_bias, batch_weight,error)
+
+        return (batch_weight)
+
+    def check_gradients(self,inp,op, dropout):
+
+        tiny = 1e-4
+        m2 = np.random.binomial(1, dropout, size=(1, 256))
+        m3 = np.random.binomial(1, dropout, size=(1, 256))
+        W1 = [256,256,256]
+        W2 = [784,256,10]
+
+        for i in range(3):
+            for j in range(W1[i]):
+                for k in range(W2[i]):
+                    np.random.seed(1)
+                    #pdb.set_trace()
+
+                    dW = self.back_Propogation(inp, op, m2, m3)
+                    gradient1 = dW[i][j,k]
+
+                    np.random.seed(1)
+
+                    self.weights[i][j,k] -= tiny
+                    probs = self.feed_Network(inp, dropout)
+                    logprob = -np.log(probs)
+                    error1 = np.dot(logprob, op)
+
+                    np.random.seed(1)
+                    self.weights[i][j,k] += 2 * tiny
+                    probs = self.feed_Network(inp, dropout)
+                    logprob = -np.log(probs)
+                    error2 = np.dot(logprob, op)
+
+                    self.weights[i][j,k] -= tiny
+
+                    gradient2 = (error2 - error1) / (2 * tiny)
+
+                    if(gradient1-gradient2[0][0] > 1e-9):
+                        #print 'incorrect'
+                        #print gradient1-gradient2[0][0]
+                        break
+
+        print "Gradients OK"
 
 
 def sigmoid(z,derivative = False):
@@ -205,11 +148,9 @@ class MNIST_load_data(object):
 
         training_inputs = [np.reshape(x, (784, 1)) for x in train[0]]
         training_results = [self.one_hot_vectors(y) for y in train[1]]
-        # training for only 10000 data set
         training_inputs = [training_inputs[i:i + 10000] for i in xrange(0, len(training_inputs), 10000)]
         training_results = [training_results[i:i + 10000] for i in xrange(0, len(training_results), 10000)]
         training_data = zip(training_inputs[0], training_results[0])
-        #pdb.set_trace()
         test_inputs = [np.reshape(x, (784, 1)) for x in test[0]]
         inputs = [test_inputs[i:i + 5000] for i in xrange(0, len(test_inputs),5000)]
         labels = [test[1][i:i + 5000] for i in xrange(0, len(test[1]), 5000)]
@@ -227,8 +168,10 @@ class MNIST_load_data(object):
 if __name__ == "__main__":
 
     mnist = MNIST_load_data()
-    dropout = 0.5 #probabity of retaining a neuron(1 is no dropout, 0 is dropping all neurons)
+    dropout = 1 #probabity of retaining a neuron(1 is no dropout, 0 is dropping all neurons)
     learning_rate = 0.01
     training_data, validation_data, testing_data = mnist.load_data_wrapper()
     network = Neural_Network()
-    network.Stochastic_Gradient_Descent(validation_data, training_data, testing_data, learning_rate, dropout)
+    for (x, y) in testing_data:
+        network.check_gradients(x,mnist.one_hot_vectors(y),dropout)
+        break
